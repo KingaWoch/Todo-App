ffc5e799c4957e6926590e299021f9c5fc4ba45c
Kinga Woch <kinga-niecko@o2.pl>
2023-04-14 10:00:52 +0200

    fixed

--- dist/app.js
+++ dist/app.js
@@ -18,54 +18,82 @@ const tasksContainer = document.querySelector(".tasks");
 const leftItems = document.querySelector(".left-items");
 const statesButtons = document.querySelector(".states-btns");
 const clearCompletedBtn = document.querySelector("#clear-completed-btn");
-// EVENT LISTENERS
-form.addEventListener("submit", addTask);
-tasksContainer.addEventListener("click", changeState);
-statesButtons.addEventListener("click", filterTasks);
-clearCompletedBtn.addEventListener("click", clearCompleted);
-countLeftItems();
-// ADD TASK FUNCTION
-function addTask(e) {
-    e.preventDefault();
-    const taskElement = document.createElement("li");
-    taskElement.classList.add("task");
-    taskElement.setAttribute("draggable", "true");
-    taskElement.innerHTML = `
+const tasks = [
+    { name: "Complete online JavaScript course", done: true },
+    { name: "Jog around the park x3", done: false },
+    { name: "10 minutes meditation", done: false },
+    { name: "Read for 1 hour", done: false },
+    { name: "Pick up groceries", done: false },
+    { name: "Complete Todo App on Frontend Mentor", done: false },
+];
+// RENDER TASK LIST
+const renderTaskList = () => {
+    tasksContainer.innerHTML = "";
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+        taskElement.classList.add("task");
+        taskElement.setAttribute("draggable", "true");
+        taskElement.innerHTML = `
     <div class="check">
-      <img src="images/icon-check.svg" alt="Mark as done" />
-    </div>
-      ${taskInput.value}
-    <button type="button" class="clear">
-      <img src="images/icon-cross.svg" alt="Clear" />
-    </button>`;
-    tasksContainer.appendChild(taskElement);
+       <img src="images/icon-check.svg" alt="Mark as done" />
+     </div>
+     ${task.name}
+     <button type="button" class="clear">
+       <img src="images/icon-cross.svg" alt="Clear" />
+     </button>
+    `;
+        // CHANGE STATE
+        if (task.done) {
+            taskElement.classList.add("completed");
+        }
+        taskElement.addEventListener("click", () => {
+            if (taskElement.classList.contains("completed")) {
+                taskElement.classList.remove("completed");
+                task.done = false;
+            }
+            else {
+                taskElement.classList.add("completed");
+                task.done = true;
+            }
+            countLeftItems();
+        });
+        tasksContainer.appendChild(taskElement);
+    });
+};
+renderTaskList();
+// ADD TASK FUNCTION
+const addTask = (task) => {
+    tasks.push(task);
+};
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addTask({ name: taskInput.value, done: false });
     taskInput.value = "";
-    // ADD TASKS TO LOCALSTORAGE
-    // saveLocalTasks(taskInput.value);
+    renderTaskList();
     countLeftItems();
+    dragAndDrop();
+    // ADD TASKS TO LOCALSTORAGE
+    //saveLocalTasks(taskInput.value);
+});
+// LEFT ITEMS
+function countLeftItems() {
+    const tasks = tasksContainer.querySelectorAll("li");
+    leftItems.innerHTML = tasks.length + " items left";
 }
-// CHANGE STATE FUNCTION
-function changeState(e) {
+countLeftItems();
+// DELETE TASK
+const deleteTask = (e) => {
     const item = e.target;
-    // DELETE TASK
+    //console.log(item);
     if (item.classList[0] === "clear") {
         const task = item.parentElement;
         task.remove();
     }
-    // MARK AS DONE
-    if (item.classList[0] === "check") {
-        const task = item.parentElement;
-        task.classList.toggle("completed");
-    }
-    if (item.classList[0] === "task") {
-        item.classList.toggle("completed");
-    }
     countLeftItems();
-}
+};
 // FILTER TASKS FUNCTION
-let tasks = tasksContainer.querySelectorAll("li");
-function filterTasks(e) {
-    console.log(tasks);
+const filterTasks = (e) => {
+    const tasks = document.querySelectorAll("li");
     tasks.forEach((task) => {
         switch (e.target.value) {
             case "all":
@@ -89,16 +117,11 @@ function filterTasks(e) {
                 break;
         }
     });
-}
-// LEFT ITEMS
-function countLeftItems() {
-    const tasks = tasksContainer.querySelectorAll("li");
-    leftItems.innerHTML = tasks.length + " items left";
-}
+    countLeftItems();
+};
 // CLEAR COMPLETED FUNCTION
 function clearCompleted(e) {
     const tasks = tasksContainer.querySelectorAll("li");
-    console.log(tasks);
     tasks.forEach((task) => {
         if (task.classList.contains("completed")) {
             task.remove();
@@ -107,25 +130,29 @@ function clearCompleted(e) {
     countLeftItems();
 }
 // DRAG AND DROP
-tasks.forEach((task) => {
-    task.addEventListener("dragstart", () => {
-        task.classList.add("dragging");
+const dragAndDrop = () => {
+    const tasks = tasksContainer.querySelectorAll("li");
+    tasks.forEach((task) => {
+        task.addEventListener("dragstart", () => {
+            task.classList.add("dragging");
+        });
+        task.addEventListener("dragend", () => {
+            task.classList.remove("dragging");
+        });
+        tasksContainer.addEventListener("dragover", (e) => {
+            e.preventDefault();
+            const afterElement = getDragAfterElement(tasksContainer, e.clientY);
+            const task = document.querySelector(".dragging");
+            if (afterElement == null) {
+                tasksContainer.appendChild(task);
+            }
+            else {
+                tasksContainer.insertBefore(task, afterElement);
+            }
+        });
     });
-    task.addEventListener("dragend", () => {
-        task.classList.remove("dragging");
-    });
-    tasksContainer.addEventListener("dragover", (e) => {
-        e.preventDefault();
-        const afterElement = getDragAfterElement(tasksContainer, e.clientY);
-        const task = document.querySelector(".dragging");
-        if (afterElement == null) {
-            tasksContainer.appendChild(task);
-        }
-        else {
-            tasksContainer.insertBefore(task, afterElement);
-        }
-    });
-});
+};
+dragAndDrop();
 function getDragAfterElement(tasksContainer, y) {
     const draggableElement = [
         ...tasksContainer.querySelectorAll(".task:not(.dragging)"),
@@ -152,3 +179,7 @@ function getDragAfterElement(tasksContainer, y) {
 //   tasks.push(task);
 //   localStorage.setItem("tasks", JSON.stringify("tasks"));
 // }
+// EVENT LISTENERS
+tasksContainer.addEventListener("click", deleteTask);
+statesButtons.addEventListener("click", filterTasks);
+clearCompletedBtn.addEventListener("click", clearCompleted);
--- src/app.ts
+++ src/app.ts
@@ -25,67 +25,100 @@ const clearCompletedBtn: HTMLButtonElement = document.querySelector(
   "#clear-completed-btn"
 );
 
-// EVENT LISTENERS
+interface Task {
+  name: string;
+  done: boolean;
+}
 
-form.addEventListener("submit", addTask);
-tasksContainer.addEventListener("click", changeState);
-statesButtons.addEventListener("click", filterTasks);
-clearCompletedBtn.addEventListener("click", clearCompleted);
-countLeftItems();
+const tasks: Task[] = [
+  { name: "Complete online JavaScript course", done: true },
+  { name: "Jog around the park x3", done: false },
+  { name: "10 minutes meditation", done: false },
+  { name: "Read for 1 hour", done: false },
+  { name: "Pick up groceries", done: false },
+  { name: "Complete Todo App on Frontend Mentor", done: false },
+];
+
+// RENDER TASK LIST
+
+const renderTaskList = () => {
+  tasksContainer.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const taskElement: HTMLElement = document.createElement("li");
+    taskElement.classList.add("task");
+    taskElement.setAttribute("draggable", "true");
+    taskElement.innerHTML = `
+    <div class="check">
+       <img src="images/icon-check.svg" alt="Mark as done" />
+     </div>
+     ${task.name}
+     <button type="button" class="clear">
+       <img src="images/icon-cross.svg" alt="Clear" />
+     </button>
+    `;
+
+    // CHANGE STATE
+
+    if (task.done) {
+      taskElement.classList.add("completed");
+    }
+    taskElement.addEventListener("click", () => {
+      if (taskElement.classList.contains("completed")) {
+        taskElement.classList.remove("completed");
+        task.done = false;
+      } else {
+        taskElement.classList.add("completed");
+        task.done = true;
+      }
+      countLeftItems();
+    });
+    tasksContainer.appendChild(taskElement);
+  });
+};
+renderTaskList();
 
 // ADD TASK FUNCTION
 
-function addTask(e: Event) {
-  e.preventDefault();
-  const taskElement: HTMLElement = document.createElement("li");
-  taskElement.classList.add("task");
-  taskElement.setAttribute("draggable", "true");
-  taskElement.innerHTML = `
-    <div class="check">
-      <img src="images/icon-check.svg" alt="Mark as done" />
-    </div>
-      ${taskInput.value}
-    <button type="button" class="clear">
-      <img src="images/icon-cross.svg" alt="Clear" />
-    </button>`;
-  tasksContainer.appendChild(taskElement);
+const addTask = (task: Task) => {
+  tasks.push(task);
+};
 
+form.addEventListener("submit", (event: Event) => {
+  event.preventDefault();
+  addTask({ name: taskInput.value, done: false });
   taskInput.value = "";
-  // ADD TASKS TO LOCALSTORAGE
-  // saveLocalTasks(taskInput.value);
+  renderTaskList();
   countLeftItems();
+  dragAndDrop();
+  // ADD TASKS TO LOCALSTORAGE
+  //saveLocalTasks(taskInput.value);
+});
+
+// LEFT ITEMS
+
+function countLeftItems() {
+  const tasks = tasksContainer.querySelectorAll("li");
+
+  leftItems.innerHTML = tasks.length + " items left";
 }
+countLeftItems();
 
-// CHANGE STATE FUNCTION
-
-function changeState(e) {
+// DELETE TASK
+const deleteTask = (e) => {
   const item: HTMLElement = e.target;
-
-  // DELETE TASK
-
+  //console.log(item);
   if (item.classList[0] === "clear") {
     const task: HTMLElement = item.parentElement;
     task.remove();
   }
-
-  // MARK AS DONE
-
-  if (item.classList[0] === "check") {
-    const task: HTMLElement = item.parentElement;
-    task.classList.toggle("completed");
-  }
-
-  if (item.classList[0] === "task") {
-    item.classList.toggle("completed");
-  }
   countLeftItems();
-}
+};
 
 // FILTER TASKS FUNCTION
-let tasks = tasksContainer.querySelectorAll("li");
 
-function filterTasks(e) {
-  console.log(tasks);
+const filterTasks = (e) => {
+  const tasks = document.querySelectorAll("li");
   tasks.forEach((task) => {
     switch (e.target.value) {
       case "all":
@@ -107,20 +140,13 @@ function filterTasks(e) {
         break;
     }
   });
-}
-
-// LEFT ITEMS
-
-function countLeftItems() {
-  const tasks = tasksContainer.querySelectorAll("li");
-  leftItems.innerHTML = tasks.length + " items left";
-}
+  countLeftItems();
+};
 
 // CLEAR COMPLETED FUNCTION
 
 function clearCompleted(e) {
   const tasks = tasksContainer.querySelectorAll("li");
-  console.log(tasks);
   tasks.forEach((task) => {
     if (task.classList.contains("completed")) {
       task.remove();
@@ -131,25 +157,29 @@ function clearCompleted(e) {
 
 // DRAG AND DROP
 
-tasks.forEach((task) => {
-  task.addEventListener("dragstart", () => {
-    task.classList.add("dragging");
+const dragAndDrop = () => {
+  const tasks = tasksContainer.querySelectorAll("li");
+  tasks.forEach((task) => {
+    task.addEventListener("dragstart", () => {
+      task.classList.add("dragging");
+    });
+    task.addEventListener("dragend", () => {
+      task.classList.remove("dragging");
+    });
+    tasksContainer.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      const afterElement = getDragAfterElement(tasksContainer, e.clientY);
+      const task = document.querySelector(".dragging");
+      if (afterElement == null) {
+        tasksContainer.appendChild(task);
+      } else {
+        tasksContainer.insertBefore(task, afterElement);
+      }
+    });
   });
-  task.addEventListener("dragend", () => {
-    task.classList.remove("dragging");
-  });
+};
 
-  tasksContainer.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    const afterElement = getDragAfterElement(tasksContainer, e.clientY);
-    const task = document.querySelector(".dragging");
-    if (afterElement == null) {
-      tasksContainer.appendChild(task);
-    } else {
-      tasksContainer.insertBefore(task, afterElement);
-    }
-  });
-});
+dragAndDrop();
 
 function getDragAfterElement(tasksContainer, y) {
   const draggableElement = [
@@ -182,3 +212,9 @@ function getDragAfterElement(tasksContainer, y) {
 //   tasks.push(task);
 //   localStorage.setItem("tasks", JSON.stringify("tasks"));
 // }
+
+// EVENT LISTENERS
+
+tasksContainer.addEventListener("click", deleteTask);
+statesButtons.addEventListener("click", filterTasks);
+clearCompletedBtn.addEventListener("click", clearCompleted);
